fc12de6b5eda5c6d39938a9e33e22805a8cc82a2
dev-verde1071 <deyessoluke689@gmail.com>
2025-11-04 18:12:32 -0500

    Create container.tsx

---
+++ src/components/container.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react";
+
+export default function Container({ children }: PropsWithChildren) {
+  return (
+    <div className="container mx-auto max-w-6xl px-4 py-8">
+      {children}
+    </div>
+  );
+}
