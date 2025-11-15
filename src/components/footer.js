ac43c176b257696f7329da56acd16ada77c2e45e
dev-verde1071 <deyessoluke689@gmail.com>
2025-11-15 12:00:17 -0500

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-brand-200 text-brand-700 py-6 mt-16 text-center">
+      <p>© {new Date().getFullYear()} Herbal Communities. All rights reserved.</p>
+    </footer>
+  );
+}
