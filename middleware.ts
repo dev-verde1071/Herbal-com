c01c76c4c87b537781b32f8ed2af44afdc5a0f33
dev-verde1071 <deyessoluke689@gmail.com>
2026-05-25 23:42:54 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isProtected = createRouteMatcher(["/dashboard(.*)", "/checkout(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtected(req)) await auth.protect();
-});
+export default function middleware() {}
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [],
 };
