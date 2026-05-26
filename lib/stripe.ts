0d4a7020ce0342de8f289f9bf1c377b20434c5d7
dev-verde1071 <deyessoluke689@gmail.com>
2026-05-25 23:49:01 -0400

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("Missing STRIPE_SECRET_KEY");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-02-24.acacia",
-});
+export const stripe = process.env.STRIPE_SECRET_KEY
+  ? new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: "2025-02-24.acacia",
+    })
+  : null;
