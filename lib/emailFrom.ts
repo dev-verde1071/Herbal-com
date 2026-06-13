26e7bde75a103a0811ab0a81e9fdea5472aa465f
dev-verde1071 <deyessoluke689@gmail.com>
2026-06-12 21:46:31 -0400

    Update emailFrom.ts

--- lib/emailFrom.ts
+++ lib/emailFrom.ts
@@ -1,11 +1,14 @@
 export const RETAIL_FROM_EMAIL =
-  process.env.RETAIL_FROM_EMAIL || "Herbal Communities <retail@herbalcommunities.com>";
+  process.env.RETAIL_FROM_EMAIL ||
+  "Herbal Communities Retail <retail@herbalcommunities.com>";
 
 export const WHOLESALE_FROM_EMAIL =
-  process.env.WHOLESALE_FROM_EMAIL || "Herbal Communities Wholesale <wholesale@herbalcommunities.com>";
+  process.env.WHOLESALE_FROM_EMAIL ||
+  "Herbal Communities Wholesale <wholesale@herbalcommunities.com>";
 
 export const RETREATS_FROM_EMAIL =
-  process.env.RETREATS_FROM_EMAIL || "Herbal Communities Retreats <retreats@herbalcommunities.com>";
+  process.env.RETREATS_FROM_EMAIL ||
+  "Herbal Communities Retreats <retreats@herbalcommunities.com>";
 
 export const DEFAULT_FROM_EMAIL =
   process.env.RESEND_FROM_EMAIL || RETAIL_FROM_EMAIL;
